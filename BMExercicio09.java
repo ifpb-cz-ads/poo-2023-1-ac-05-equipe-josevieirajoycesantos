0b124c092c51de8c2272e608f385272084883cc6
Joyce Santos <119745348+Joyce-Firmino@users.noreply.github.com>
2023-04-12 14:40:19 -0300

    Questão 09

---
+++ BMExercicio09.java
@@ -0,0 +1,11 @@
+public class BMExercicio09 {
+    public static void main(String[] args){
+        int x=10;
+        int contador=0;
+        while(contador<=20){
+            System.out.println(x);
+            x++;
+            contador++;
+        }
+    }
+}
