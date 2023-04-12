7fff04281f384ce96a25590303baf48b2b66fc6b
Joyce Santos <119745348+Joyce-Firmino@users.noreply.github.com>
2023-04-12 14:41:05 -0300

    Adicionando a questão 10

---
+++ BMExercicio10.java
@@ -0,0 +1,11 @@
+public class BMExercicio10 {
+    public static void main(String[] args){
+        int x=10;
+        int contador=0;
+        do {
+            System.out.println(x);
+            x++;
+            contador++; 
+        } while (contador<=20);
+    }
+}
